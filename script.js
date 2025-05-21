bbd4873ac7ad02aeddffac58a51d5c34e5629100
alumno <alumno@Instituto.local>
2025-05-21 08:23:35 -0300

    avanacesclase
    Terminar Ej; 3 y hacer hasta el 5

---
+++ script.js
@@ -0,0 +1,17 @@
+//Ejercicio 1 Crear y modificar arreglos
+
+let frutas = ['Pera', 'Manzana', 'Banana']
+frutas.push('Uva')
+frutas.unshift('Mango')
+frutas.pop()
+frutas.shift()
+console.log(frutas)
+//Ejercicio 2  Contar elementos
+
+let nombres = ['Facu', 'Benja', 'Alejo']
+console.log(nombres.length)
+nombres.push('Laura','Luis')
+console.log(nombres)
+
+//Ejercicio 3 
+let numeros = [0, 1, 2, 3]
