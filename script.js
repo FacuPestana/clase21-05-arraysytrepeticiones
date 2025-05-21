1e6c883a539276984e4a76ae55f2ca17a2d29382
Facu Pestana <136341998+FacuPestana@users.noreply.github.com>
2025-05-21 15:13:53 -0300

    Listo!!
    Terminado!

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 //Ejercicio 1 Crear y modificar arreglos
 
 let frutas = ['Pera', 'Manzana', 'Banana']
@@ -6,12 +7,36 @@ frutas.unshift('Mango')
 frutas.pop()
 frutas.shift()
 console.log(frutas)
+
 //Ejercicio 2  Contar elementos
 
 let nombres = ['Facu', 'Benja', 'Alejo']
 console.log(nombres.length)
-nombres.push('Laura','Luis')
+nombres.push('Cristian','Luis')
 console.log(nombres)
 
-//Ejercicio 3 
-let numeros = [0, 1, 2, 3]
+//Ejercicio 3  Recorrer un arreglo con for
+
+let num = [0, 1, 2, 3];
+for (let i = 0; i < num.length; i++) {
+  console.log(num[i]);
+}
+
+//Ejercicio 4 Sumar elementos de un arreglo
+
+let numeros = [2, 4, 10, 15]
+let suma = 0;
+for (let i = 0; i < numeros.length; i++) {
+  suma += numeros[i]; 
+}
+console.log("La suma total es:", suma);
+
+//Ejercicio 5 Cargar elementos con prompt()
+alert('A continuación se te pedirá que ingreses 5 palabras');
+let palabras = [];
+for (let i = 0; i < 5; i++) {
+
+  let palabra = prompt("Escribe una palabra:");
+  palabras.push(palabra);
+}
+console.log("Las palabras que escribiste son:", palabras);
